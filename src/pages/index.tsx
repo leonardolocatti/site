0def88a888f2d389515310f905faa1035270bcac
Leonardo Locatti <locatti.leonardo@gmail.com>
2021-04-19 20:50:14 -0300

    Finalizada página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import styles from '../styles/pages/home.module.scss';
+
 export default function Home(): JSX.Element {
-  return <>site</>;
+  return (
+    <>
+      <Head>
+        <title>Home | Leonardo Locatti - Dev Web</title>
+      </Head>
+
+      <main className={styles.homeContainer}>
+        <section className={styles.homeContent}>
+          <h1>Leonardo Locatti</h1>
+
+          <span>Desenvolvedor Web</span>
+
+          <Link href="/contact">
+            <a>Entrar em contato</a>
+          </Link>
+        </section>
+
+        <img src="/images/profile.jpeg" alt="Leonardo Locatti" />
+      </main>
+    </>
+  );
 }
